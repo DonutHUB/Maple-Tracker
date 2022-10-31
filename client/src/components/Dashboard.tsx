4993ab5d43de41f6d15332a52b74805ba0917416
DonutHUB <36317067+DonutHUB@users.noreply.github.com>
2022-10-31 00:40:02 -0400

    latest

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -3,6 +3,7 @@ import Tasks from "./Tasks";
 
 export default function Dashboard() {
   const [dailies, setDailies] = useState<number>(0);
+  const [weeklies, setWeeklies] = useState<number>(0);
 
   return (
     <div className="content">
@@ -12,7 +13,15 @@ export default function Dashboard() {
         setMesos={setDailies}
         mesos={dailies}
       ></Tasks>
-      <div>{dailies}</div>
+      <Tasks
+        json="./Weekly Bosses.json"
+        header="Weeklies"
+        setMesos={setWeeklies}
+        mesos={weeklies}
+      ></Tasks>
+      <div>Dailies: {7*dailies}</div>
+      <div>Weeklies: {weeklies}</div>
+      <div>Total: {7*dailies + weeklies}</div>
     </div>
   );
 }
