2da8ffb643a9bccb365444130f02ca4dfc0acef5
TsyooHUB <36317069+TsyooHUB@users.noreply.github.com>
2022-10-30 23:38:54 -0400

    Update Tasks.tsx

--- client/src/components/Tasks.tsx
+++ client/src/components/Tasks.tsx
@@ -1,18 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Tasks() {
-  const [info, setInfo] = useState();
+  const [info, setInfo] = useState<{ name: any; mesos: any; image: any }[]>();
 
-  const dailyBossesToHTML = async () => {
-    const file = await fetch("./Daily Bosses.json");
-    const daily_bosses = await file.json();
-    setInfo(daily_bosses);
-    const rows = daily_bosses.map((result: { image: any; name: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; mesos: string | number | boolean | {} | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactNodeArray | React.ReactPortal | null | undefined; }, index: React.Key | null | undefined) => (
+  useEffect(() => {
+    const fetchData = async () => {
+      const file = await fetch("./Daily Bosses.json");
+      const daily_bosses = await file.json();
+      setInfo(daily_bosses);
+    };
+    fetchData();
+  }, [info]);
+
+  const dailyBossesToHTML = () => {
+    if (!info) return "";
+    const rows = info?.map(
+      (
+        result: {
+          image: any;
+          name:
+            | boolean
+            | React.ReactChild
+            | React.ReactFragment
+            | React.ReactPortal
+            | null
+            | undefined;
+          mesos:
+            | string
+            | number
+            | boolean
+            | {}
+            | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+            | React.ReactNodeArray
+            | React.ReactPortal
+            | null
+            | undefined;
+        },
+        index: React.Key | null | undefined
+      ) => (
         <div key={index}>
           <div className="columns">
             <div className="column has-text-centered">
               <input type={"checkbox"} />
-              <img src={`./${result.image}`} style={{height: "100px"}}></img>
+              <img src={`./${result.image}`} style={{ height: "100px" }}></img>
             </div>
             <div className="column has-text-centered">
               <div>{result.name}</div>
@@ -20,22 +50,17 @@ export default function Tasks() {
             </div>
           </div>
         </div>
-    ));
+      )
+    );
 
-      return rows;
+    return rows;
   };
 
-  return info ? (
+  return (
     <div className="content has-text-centered">
       <div className="block">
         <h4>Dailies</h4>
         {dailyBossesToHTML()}
-      </div>
-    </div>
-  ) : (
-    <div className="content has-text-centered">
-      <div className="block">
-        <h4>boo</h4>
       </div>
     </div>
   );
